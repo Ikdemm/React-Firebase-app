a6d7c9f94c518e9b5a7818d77cb724d7ea457f59
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-10-15 18:52:51 +0100

    implemented sessions

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,8 @@
 // Dependencies imports
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { withFirebase } from '../Firebase';
+import { AuthUserContext } from '../Session'
 
 // Components imports
 import Navigation from '../Navigation';
@@ -13,46 +15,27 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
+import { withAuthentication } from '../Session'; 
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
  
-class App extends Component {
+      <hr />
+ 
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route
+        path={ROUTES.PASSWORD_FORGET}
+        component={PasswordForgetPage}
+      />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
+)
 
-  constructor(props) {
-    super(props)
-    // By default, we have no authenticated user
-    this.state = {
-      authUser: null
-    }
-  }
-
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
-      authUser
-        ? this.setState({ authUser })
-        : this.setState({ authUser: null });
-    });
-  }
-
-  componentWillUnmount() {
-    this.listener();
-  }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navigation authUser={this.state.authUser}/>
-          <hr />
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-          <Route path={ROUTES.HOME} component={HomePage} />
-          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-          <Route path={ROUTES.ADMIN} component={AdminPage} />
-        </div>
-      </Router>
-    );
-  }
-}
-
-export default withFirebase(App);
+export default withAuthentication(App);
