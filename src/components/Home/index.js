0ac3c6736d3877cccda07ff8393efed0702021f6
Ikdemm <ikdem.benmbarek@aiesec.net>
2020-10-16 14:09:30 +0100

    styled everything

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import landing from '../../images/landing.png'
  
 const Home = () => (
   <div>
-    <h1>Home</h1>
+    <div className="container landing-container">
+      <h1 className="page-title landing-title">Your Authentication works </h1>
+      <p>Thanks to this <a href="https://www.robinwieruch.de/complete-firebase-authentication-react-tutorial" class="book-link">dude</a></p>
+      <img src={landing} alt="react-firebase" width="800px"/>
+    </div>
   </div>
 );
  
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import landing from '../../images/landing.png'
  
 const Landing = () => (
-  <div>
-    <h1>Landing</h1>
+  <div className="container landing-container">
+    <h1 className="page-title landing-title">Authentication with 
+    <a href="https://reactjs.org/" class="react"> React </a> 
+    and 
+    <a href="https://firebase.google.com/" class="firebase"> Firebase </a>
+    </h1>
+    <p>Inspired by this <a href="https://www.robinwieruch.de/complete-firebase-authentication-react-tutorial" class="book-link">dude</a></p>
+    <img src={landing} alt="react-firebase" width="800px"/>
   </div>
 );
- 
+  
 export default Landing;
